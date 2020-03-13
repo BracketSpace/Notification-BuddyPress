e43aea676d736771f12add57e99e0d3f5dff2a60
Tomasz Adamowicz <tomasz@bracketspace.com>
2020-03-13 12:10:33 +0100

    dump hooks

--- src/includes/hooks.php
+++ src/includes/hooks.php
@@ -8,6 +8,4 @@
  */
 
 // phpcs:disable
-add_action( 'init', [ $this->component( 'i18n' ), 'load_translation' ], 10, 0 );
-add_filter( 'notification/scripts/allowed_hooks', [ $this->component( 'scripts' ), 'add_screen' ], 10, 1 );
-add_action( 'notification/scripts', [ $this->component( 'scripts' ), 'enqueue_scripts' ], 10, 1 );
+
